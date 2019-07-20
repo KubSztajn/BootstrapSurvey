67f041b6c28b5742f53a0c139b1fa5eda326344b
KubSztajn <43608386+KubSztajn@users.noreply.github.com>
2019-07-21 01:15:18 +0200

    Added questions form and some little improvements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+//Add choice input function
+document.getElementById('add-choice').onclick = function(parentId, elementId, elementTag, html) {
+    let p = document.getElementById('choices');
+    let newElement = document.createElement(elementTag);
+    newElement.setAttribute('id', elementId);
+    html =  '<input style="margin-top: 20px;" id="added" type="text" class="form-control d-inline" placeholder="Enter choice">';
+    newElement.innerHTML = html;
+    p.appendChild(newElement);
+};
+
+//Remove choice input function
+document.getElementById('remove-choice').onclick = function() {
+    let addedInput = document.getElementById('added');
+    addedInput.parentNode.removeChild(addedInput);
+};
+
 
---
+++ js/nav.js
@@ -0,0 +1,24 @@
+let nav = '<a class="navbar-brand nav-link"><img class="d-block" width="43" height="43" src="img/logo.png"></a>\n' +
+    '\n' +
+    '<button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">\n' +
+    '<span class="navbar-toggler-icon"></span>\n' +
+    '</button>\n' +
+    '\n' +
+    '<div class="collapse navbar-collapse" id="collapsibleNavbar">\n' +
+    '<ul class="navbar-nav">\n' +
+    '<li class="nav-item">\n' +
+    '<a href="index.html" class="nav-link" style="margin-left: 30px;font-size:2em;">Home</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '<a href="#" class="nav-link" style="margin-left: 30px;font-size:2em;">About</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '<a href="contact.html" class="nav-link" style="margin-left: 30px;font-size:2em;">Contact</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '<a href="login.html" class="nav-link" style="margin-left: 30px;font-size:2em;">Login</a>\n' +
+    '</li>\n' +
+    '</ul>\n' +
+    '</div>';
+
+document.getElementById('navbar').innerHTML = nav;
